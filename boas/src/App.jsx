9992485533d2b514bf3fd0d795c04513d51cc7f9
rodrigo01999 <flashrapido384@gmail.com>
2025-04-08 12:22:41 -0300

    08/04

---
+++ boas/src/App.jsx
@@ -0,0 +1,17 @@
+import BoasVindas from "./exemplo1"
+
+function App() {
+  
+
+  return (
+<div>
+  <h1>Boas Vindas ao React com Java Script</h1>
+  <p>Esse é o arquivo App.js que será o nosso ponto de partida para o desenvolvimento de aplicativo React utilizando JavaScript</p>
+
+  <BoasVindas nome="Rodrigo" />
+</div>
+  )
+    
+}
+
+export default App
